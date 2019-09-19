b19552ac20c050a9658700dd685ca97ee727d6e1
Vonrroker <dayhamsoares@gmail.com>
2019-09-19 19:51:22 -0300

    return included file model when create meettup

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -3,6 +3,7 @@ import { isBefore, parseISO, startOfDay, endOfDay } from 'date-fns';
 import { Op } from 'sequelize';
 import Meetup from '../models/Meetup';
 import User from '../models/User';
+import File from '../models/File';
 
 class MeetupController {
   async index(req, res) {
@@ -68,6 +69,15 @@ class MeetupController {
       locale,
       title,
       date,
+    });
+
+    await meetup.reload({
+      include: [
+        {
+          model: File,
+          as: 'file',
+        },
+      ],
     });
 
     return res.json(meetup);
