3325882fcf74c3f103fa5af52cd192772f066ed8
Vonrroker <dayhamsoares@gmail.com>
2019-10-25 16:20:11 -0300

    midleware fix return

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -15,7 +15,7 @@ export default async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
     req.userId = decoded.id;
   } catch (error) {
-    res.status(401).json({ error: 'Token invalid' });
+    return res.status(401).json({ error: 'Token invalid' });
   }
 
   return next();
